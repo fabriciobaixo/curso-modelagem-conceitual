fbdcc445ef750556eb7485f2b02f6c58d2127fd9
Fabricio Baixo <fabriciobaixo@gmail.com>
2019-08-27 17:49:01 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/fabriciobaixo/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/fabriciobaixo/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -26,5 +26,6 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
 
 }
