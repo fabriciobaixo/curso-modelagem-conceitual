e92a349b42e612fdd613b0e2a397d60ade3fc741
Fabricio Baixo <fabriciobaixo@gmail.com>
2019-08-29 18:24:23 -0300

    Subtotal e total do Pedido

--- src/main/java/com/fabriciobaixo/cursomc/domain/ItemPedido.java
+++ src/main/java/com/fabriciobaixo/cursomc/domain/ItemPedido.java
@@ -10,19 +10,23 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
-	public ItemPedido(){
+
+	public ItemPedido() {
 	}
-	
-	public ItemPedido(Pedido pedido,Produto produto, Double desconto, Integer quantidade, Double preco) {
+
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
 		id.setPedido(pedido);
 		id.setProduto(produto);
@@ -35,8 +39,7 @@ public class ItemPedido implements Serializable {
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
-	
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
@@ -45,36 +48,29 @@ public class ItemPedido implements Serializable {
 		return id;
 	}
 
-
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
-
 
 	public Double getDesconto() {
 		return desconto;
 	}
 
-
 	public void setDesconto(Double desconto) {
 		this.desconto = desconto;
 	}
-
 
 	public Integer getQuantidade() {
 		return quantidade;
 	}
 
-
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
 
-
 	public Double getPreco() {
 		return preco;
 	}
-
 
 	public void setPreco(Double preco) {
 		this.preco = preco;
@@ -104,7 +100,5 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
+
 }
